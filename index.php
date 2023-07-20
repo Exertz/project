3ee6c53dc6e8d2df4fa23010de27d48fcd90569b
Cedrick Noel Dangcalan <dnacd123@gmail.com>
2023-07-20 15:30:37 +0800

    a

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <meta charset="UTF-8"/>
+       <meta htpp-equive="X-UA-Compatible" content="IE=edge"/>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+       <link rel="stylesheet" href="style.css"/>
+       <title></title> 
+    </head>
+    <body>
+        <header>
+        <nav style="background-color: #8AC4FF;">
+    <div class="navbar">
+        <div class="logo">
+            <img src="./images/Logo.png" alt="" width="100px"/>
+        </div>
+        <ul class="links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a href="#" class="action_btn">Book now</a>
+        <div class="toggle_btn"></div>
+        </div>
+        </nav>
+        </header>
+        <p>Transform Your Vision into Reality:
+        REGISTER Now! 
+        to Book Unforgettable 
+        Photo and Video Experiences</p>
+        <div>
+        <a href="#" class="register_btn">Register</a>
+        <div class="toggle_btn:register"></div>
+        </div>
+        <div class="image-container">
+        <img src="./images/wave.png">
+        </div>
+    </body>
+    </html>
